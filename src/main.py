f8f376934784075b5f295d9df5830bf6c1f5ad6a
Rocha Lima <jonasrla@gmail.com>
2017-09-14 00:59:31 -0300

    Implements main w/ data cleaning

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from clear import augment_table, tokenize
+import numpy as np
+
+table = pd.read_csv('data/train.csv')
+
+# Data cleaning
+table = augment_table(table)
+table.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
+table = tokenize(table, ['Pclass',
+                         'Sex',
+                         'SibSp',
+                         'Parch',
+                         'Embarked',
+                         'Title',
+                         'Deck'])
+
+table.loc[np.isnan(table.Age)].Age = 29.6991
+
+# Build model
+
+# Classify
