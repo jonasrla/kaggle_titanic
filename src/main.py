c742e593f7eb73470c7cda38456a92ec80784949
Rocha Lima <jonasrla@gmail.com>
2017-09-17 18:28:41 -0300

    Refactor

--- src/main.py
+++ src/main.py
@@ -4,15 +4,15 @@ from sklearn.preprocessing import LabelEncoder
 from clear import augment_table, tokenize
 import numpy as np
 
-table = pd.read_csv('data/train.csv')
+train_table = pd.read_csv('data/train.csv')
 
-m = table.shape[0]
+m = train_table.shape[0]
 train_size = (m*4)/5
 # Data cleaning
-table.loc[np.isnan(table.Age), 'Age'] = np.mean(table.Age)
-table = augment_table(table)
-table.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
-table = tokenize(table, ['Pclass',
+train_table.loc[np.isnan(train_table.Age), 'Age'] = np.mean(train_table.Age)
+train_table = augment_table(train_table)
+train_table.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
+train_table = tokenize(train_table, ['Pclass',
                          'Sex',
                          'SibSp',
                          'Parch',
@@ -20,8 +20,8 @@ table = tokenize(table, ['Pclass',
                          'Title',
                          'Deck'])
 
-Y = table.Survived.as_matrix()
-X = table.drop('Survived', axis=1).as_matrix()
+Y = train_table.Survived.as_matrix()
+X = train_table.drop('Survived', axis=1).as_matrix()
 
 index = np.arange(m)
 np.random.shuffle(index)
@@ -29,14 +29,14 @@ np.random.shuffle(index)
 X_train = X[index[:train_size]]
 Y_train = Y[index[:train_size]]
 
-X_test = X[index[train_size:]]
-Y_test = Y[index[train_size:]]
+X_val = X[index[train_size:]]
+Y_val = Y[index[train_size:]]
 # Build model
 rfc = RandomForestClassifier()
 rfc.fit(X_train, Y_train)
 
 # Classify
-Y_hat = rfc.predict(X_test)
+Y_hat = rfc.predict(X_val)
 
-prec = np.sum(Y_test==Y_hat, dtype=np.float64)/(m-train_size)
+prec = np.sum(Y_val==Y_hat, dtype=np.float64)/(m-train_size)
 print "Precision rate: {}".format(prec)
