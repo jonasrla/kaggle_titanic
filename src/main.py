f0a636d53a89bff328be4f7674cdf1bbed6cb16d
Rocha Lima <jonasrla@gmail.com>
2017-09-17 18:10:20 -0300

    Adds classifier

--- src/main.py
+++ src/main.py
@@ -20,8 +20,23 @@ table = tokenize(table, ['Pclass',
                          'Title',
                          'Deck'])
 
-table.loc[np.isnan(table.Age)].Age = 29.6991
+Y = table.Survived.as_matrix()
+X = table.drop('Survived', axis=1).as_matrix()
 
+index = np.arange(m)
+np.random.shuffle(index)
+
+X_train = X[index[:train_size]]
+Y_train = Y[index[:train_size]]
+
+X_test = X[index[train_size:]]
+Y_test = Y[index[train_size:]]
 # Build model
+rfc = RandomForestClassifier()
+rfc.fit(X_train, Y_train)
 
 # Classify
+Y_hat = rfc.predict(X_test)
+
+prec = np.sum(Y_test==Y_hat, dtype=np.float64)/(m-train_size)
+print "Precision rate: {}".format(prec)
