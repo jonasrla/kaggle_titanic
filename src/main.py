40ce29b4b0a47a54108f1b0f2d29d218b32772cc
Rocha Lima <jonasrla@gmail.com>
2017-09-19 23:09:56 -0300

    Adds test dataset

--- src/main.py
+++ src/main.py
@@ -5,24 +5,34 @@ from clear import augment_table, tokenize
 import numpy as np
 
 train_table = pd.read_csv('data/train.csv')
+test_table = pd.read_csv('data/test.csv')
+label_test = pd.DataFrame(test_table.PassengerId)
 
 m = train_table.shape[0]
 train_size = (m*4)/5
 # Data cleaning
 train_table.loc[np.isnan(train_table.Age), 'Age'] = np.mean(train_table.Age)
+test_table.loc[np.isnan(test_table.Age), 'Age'] = np.mean(train_table.Age)
+test_table.loc[np.isnan(test_table.Fare), 'Fare'] = np.mean(train_table.Fare)
+
 train_table = augment_table(train_table)
+test_table = augment_table(test_table)
+
 train_table.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
-train_table = tokenize(train_table, ['Pclass',
-                         'Sex',
-                         'SibSp',
-                         'Parch',
-                         'Embarked',
-                         'Title',
-                         'Deck'])
+test_table.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
+
+train_table, test_table = tokenize(train_table, test_table,
+                                   ['Sex',
+                                   'SibSp',
+                                   'Parch',
+                                   'Embarked',
+                                   'Title',
+                                   'Deck'])
 
 Y = train_table.Survived.as_matrix()
 X = train_table.drop('Survived', axis=1).as_matrix()
 
+X_test = test_table.as_matrix()
 index = np.arange(m)
 np.random.shuffle(index)
 
